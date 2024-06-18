5caac31d537d7aec148a1fd6b11ac40f2d4e9ee3
Waluyo Ade Prasetio <adeprastio1996@gmail.com>
2024-06-18 19:30:49 +0700

    init project springboot web

---
+++ src/main/java/com/github/abdullahprasetio/DemoApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.abdullahprasetio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiApplication.class, args);
+	}
+
+}
