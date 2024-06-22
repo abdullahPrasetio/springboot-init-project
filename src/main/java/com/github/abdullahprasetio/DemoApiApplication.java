a7cd7d74d9121824ead13175a80aa63f31a97abd
Waluyo Ade Prasetio <adeprastio1996@gmail.com>
2024-06-22 18:09:43 +0700

    feat: add swagger and config swagger ui

--- src/main/java/com/github/abdullahprasetio/DemoApiApplication.java
+++ src/main/java/com/github/abdullahprasetio/DemoApiApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.ui.ModelMap;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
 @SpringBootApplication
 public class DemoApiApplication {
 
@@ -17,5 +23,18 @@ public class DemoApiApplication {
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
 	}
+
+	@Bean
+  	public OpenAPI springShopOpenAPI() {
+      return new OpenAPI()
+              .info(new Info().title("Springboot MyAPI By Temancode")
+              .description("Spring sample application")
+              .version("v0.0.1")
+			  .contact(new Contact().name("Waluyo Ade Prasetio").email("temancode@gmail.com").url("https://github.com/abdullahPrasetio"))
+              .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+              .externalDocs(new ExternalDocumentation()
+              .description("Temancode repository")
+              .url("https://github.com/abdullahPrasetio"));
+  }
 
 }
