4f279cd6d1975871c1fe241c276e8d4c61a24654
Waluyo Ade Prasetio <adeprastio1996@gmail.com>
2024-06-25 20:55:01 +0700

    feat: add project lombok in category entity

--- src/main/java/com/github/abdullahprasetio/models/entities/Category.java
+++ src/main/java/com/github/abdullahprasetio/models/entities/Category.java
@@ -8,10 +8,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "tbl_categories")
-public class Category extends BaseEntity<String> implements Serializable{
+public @Data class Category extends BaseEntity<String> implements Serializable{
     
         private static final long serialVersionUID = 1L;
     
@@ -22,29 +23,6 @@ public class Category extends BaseEntity<String> implements Serializable{
         @Column(length = 100,nullable = false,unique = true)
         private String name;
     
-        public Category(Long id, String name) {
-            this.id = id;
-            this.name = name;
-        }
-
-        public Category() {
-        }
-
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
         
     
 }
